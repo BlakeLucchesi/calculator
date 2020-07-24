00361ca0a4c9c3b41acad0babceedc4c34795ee8
Blake Lucchesi <blakelucchesi@fb.com>
2020-07-24 13:29:48 -0700

    Initial Commit

---
+++ calculator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  calculator
+//
+//  Created by blakelucchesi on 7/24/20.
+//  Copyright © 2020 nonya. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
